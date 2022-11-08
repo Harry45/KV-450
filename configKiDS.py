5555fb0a09b38c469a82859e1dc03b69dd1e5dd7
Harry45 <arrykrish@gmail.com>
2022-11-08 05:45:14 +0000

    KV-450

--- configKiDS.py
+++ configKiDS.py
@@ -3,7 +3,7 @@ Authors: Arrykrishna Mootoovaloo and George Kyriacou
 Supervisors: Alan Heavens, Andrew Jaffe, Florent Leclercq
 Date: November 2022
 Project: KiDS+VIKING-450 cosmology with Bayesian hierarchical model redshift distributions
-Script: Contains all the configurations required for running the code
+Script: Contains all the configurations required for running the code (KV-450)
 """
 
 import ml_collections
---
+++ configSIM.py
@@ -0,0 +1,7 @@
+"""
+Authors: Arrykrishna Mootoovaloo and George Kyriacou
+Supervisors: Alan Heavens, Andrew Jaffe, Florent Leclercq
+Date: November 2022
+Project: KiDS+VIKING-450 cosmology with Bayesian hierarchical model redshift distributions
+Script: Contains all the configurations required for running the code (Simulations)
+"""
