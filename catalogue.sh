420387460bb3f27cf7e0aa714c2be58f6cab8bd2
Harry45 <arrykrish@gmail.com>
2022-11-12 08:10:03 +0000

    catalogues

---
+++ catalogue.sh
@@ -0,0 +1,5 @@
+wget -O data/catlogue/KV450_G9_reweight_3x4x4_v2_good.cat http://ds.astro.rug.astro-wise.org:8000/KV450_G9_reweight_3x4x4_v2_good.cat
+wget -O data/catlogue/KV450_G12_reweight_3x4x4_v2_good.cat http://ds.astro.rug.astro-wise.org:8000/KV450_G12_reweight_3x4x4_v2_good.cat
+wget -O data/catlogue/KV450_G15_reweight_3x4x4_v2_good.cat http://ds.astro.rug.astro-wise.org:8000/KV450_G15_reweight_3x4x4_v2_good.cat
+wget -O data/catlogue/KV450_G23_reweight_3x4x4_v2_good.cat http://ds.astro.rug.astro-wise.org:8000/KV450_G23_reweight_3x4x4_v2_good.cat
+wget -O data/catlogue/KV450_GS_reweight_3x4x4_v2_good.cat http://ds.astro.rug.astro-wise.org:8000/KV450_GS_reweight_3x4x4_v2_good.cat
